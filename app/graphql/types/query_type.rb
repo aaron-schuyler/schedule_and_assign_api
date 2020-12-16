73e33e0e1999e2d9b24ad9e236d846da6ace27a4
aaron-schuyler <asky28@gmail.com>
2020-12-15 23:01:12 -0500

    graph ql select by for teams

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,13 +5,43 @@ module Types
       null: false,
       description: 'Returns a list of games'
 
-      field :teams,
-        [Types::TeamType],
-        null: false,
-        description: 'Returns a list of teams'
+    field :teams,
+      [Types::TeamType],
+      null: false,
+      description: 'Returns a list of teams'
+
+    field :levels,
+      [Types::LevelType],
+      null: false,
+      description: 'Returns a list of levels'
+
+    field :organizations,
+      [Types::OrganizationType],
+      null: false,
+      description: 'Returns a list of organizations'
+
+    field :team,
+      Types::TeamType,
+      null: false do
+        argument :id, ID, required: false
+        argument :organization_id, ID, required: false
+        argument :level_id, ID, required: false
+      end
 
     def games
       Game.all
+    end
+    def teams
+      Team.all
+    end
+    def team(args)
+      Team.find_by(args)
+    end
+    def levels
+      Level.all
+    end
+    def organizations
+      Organization.all
     end
   end
 end
